b711c31586fb4af89b0b13679d36b1d92d669b0d
lamsila <mouad.lamssila@gmail.com>
2025-03-14 10:30:53 +0000

    responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin, FaCcVisa, FaCcPaypal, F
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-10 mt-10">
+    <footer className="bg-gray-900 px-7 text-white py-10 mt-10">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 text-center md:text-left">
         
         <div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [search, setSearch] = useState("");
@@ -10,11 +10,12 @@ export default function Navbar() {
     console.log("Recherche :", search);
   };
 
+  
   return (
-    <nav className="bg-green-100 text-white p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className={`bg-green-100 sm:h-auto ${isMenuOpen? 'fixed inset-0 z-50':''}   text-white p-4 shadow-md`}>
+      <div className="container relative mx-auto  flex justify-between items-center">
         <Link to="/">
-          <img src="/logo1.jpeg" alt="BioBazar" className="h-14 w-20 logo" />
+          <img src="/logo1.jpeg" alt="BioBazar" className={`${isMenuOpen ? "hidden" : ""} h-14 w-20 logo `} />
         </Link>
 
         <div className="relative w-1/3 hidden md:block">
@@ -31,25 +32,31 @@ export default function Navbar() {
         </div>
 
         <button
-          className="md:hidden text-gray-600"
+          className={` ${isMenuOpen ? "hidden" : ""} md:hidden text-gray-600`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          ☰
+        </button>
+        <button
+          className={` ${isMenuOpen ? "" : "hidden"} md:hidden top-0 right-0 px-4 absolute text-gray-600`}
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           ☰
         </button>
 
-        <ul className={`md:flex space-x-4 ${isMenuOpen ? "block" : "hidden"} md:block`}>
+        <ul className={`md:flex flex sm:flex-row flex-col gap-7 sm:w-auto w-full sm:h-auto h-[100vh] justify-center items-center    ${isMenuOpen ? "block" : "hidden"} md:block`}>
           <li>
-            <Link to="/" className="text-green-500 hover:underline">
+            <Link to="/" className="text-green-500 w-full text-center flex justify-center  hover:underline">
               Accueil
             </Link>
           </li>
           <li>
-            <Link to="/produits" className=" text-green-500 hover:underline">
+            <Link to="/produits" className=" text-green-500 w-full flex justify-center    hover:underline">
               Produits
             </Link>
           </li>
           <li>
-            <Link to="/Seconnecter" className="text-green-500 hover:underline">
+            <Link to="/Seconnecter" className="text-green-500  flex justify-center  w-full text-center hover:underline">
               Se connecter
             </Link>
           </li>
